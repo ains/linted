de8e4fb9e574e608228357da87a9a4bb7737981a
Ainsley Escorce-Jones <me@ains.co>
2014-03-21 20:39:39 +0000

    Added defined sniffs to phpcs ruleset generator

--- scanners/phpcs/generate_ruleset.py
+++ scanners/phpcs/generate_ruleset.py
@@ -9,6 +9,7 @@ import json
 import glob
 import collections
 
+from scanners.settings import tree
 
 def get_rule_data():
     #Allows us to silently handle non documented sniffs by returning empty strings
@@ -43,9 +44,31 @@ if __name__ == '__main__':
         exit(0)
 
     standards_path = sys.argv[1]
-    ruleset = {}
+    ruleset = tree()
 
     rule_data = get_rule_data()
+
+    #Get defined sniffs
+    for doc_file in glob.glob(os.path.join(sys.argv[1], '*', 'Sniffs', '*', '*.php')):
+        #Get rule from doc path
+        rel_path = os.path.relpath(doc_file, standards_path)
+        rule_name = rel_path \
+            .replace('/Sniffs', '') \
+            .replace('.php', '') \
+            .replace('Sniff', '') \
+            .replace('/', '.')
+
+        ruleset_name = rule_name.split('.')[0]
+
+        ruleset[ruleset_name]['name'] = rule_name
+        ruleset[ruleset_name]['rules'][rule_name] = {
+            'name': rule_data[rule_name]['name'],
+            'enabled': True,
+            'description': rule_data[rule_name]['description'],
+            'properties': {}
+        }
+
+    #Get imported rules
     for root, _, filenames in os.walk(sys.argv[1]):
         for ruleset_file in fnmatch.filter(filenames, 'ruleset.xml'):
 
@@ -54,23 +77,18 @@ if __name__ == '__main__':
 
             ruleset_file_name = os.path.basename(ruleset_file)
             ruleset_name = root.get('name')
-            ruleset_rules = {}
 
+            ruleset[ruleset_name]['name'] = ruleset_name
             for rule_node in root.findall('rule'):
                 rule_name = rule_node.get('ref')
 
                 #Only pick "top-level" rules
                 if rule_name.count('.') == 2:
-                    ruleset_rules[rule_name] = {
+                    ruleset[ruleset_name]['rules'][rule_name] = {
                         'name': rule_data[rule_name]['name'],
                         'enabled': True,
                         'description': rule_data[rule_name]['description'],
                         'properties': {}
                     }
-
-            ruleset[ruleset_name] = {
-                'name': ruleset_name,
-                'rules': ruleset_rules
-            }
 
     print(json.dumps(ruleset))
