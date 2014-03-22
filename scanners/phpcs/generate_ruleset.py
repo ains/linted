ee79c94b0b0626dbedd3536419ab14289b0fc0f9
Ainsley Escorce-Jones <me@ains.co>
2014-03-22 21:00:15 +0000

    refactored phpcs ruleset generation

--- scanners/phpcs/generate_ruleset.py
+++ scanners/phpcs/generate_ruleset.py
@@ -3,13 +3,11 @@
 
 import sys
 import os
-import fnmatch
 import xml.etree.ElementTree as ElementTree
-import json
 import glob
 import collections
+import json
 
-from scanners.settings import tree
 
 def get_rule_data():
     #Allows us to silently handle non documented sniffs by returning empty strings
@@ -38,18 +36,9 @@ def get_rule_data():
     return rule_data
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Please call the script with the directory containing the PHPCS standards')
-        exit(0)
-
-    standards_path = sys.argv[1]
-    ruleset = tree()
-
-    rule_data = get_rule_data()
-
-    #Get defined sniffs
-    for doc_file in glob.glob(os.path.join(sys.argv[1], '*', 'Sniffs', '*', '*.php')):
+def get_defined_sniffs(standard_dir, rule_data):
+    defined_sniffs = {}
+    for doc_file in glob.glob(os.path.join(standard_dir, 'Sniffs', '*', '*.php')):
         #Get rule from doc path
         rel_path = os.path.relpath(doc_file, standards_path)
         rule_name = rel_path \
@@ -58,37 +47,65 @@ if __name__ == '__main__':
             .replace('Sniff', '') \
             .replace('/', '.')
 
-        ruleset_name = rule_name.split('.')[0]
-
-        ruleset[ruleset_name]['name'] = rule_name
-        ruleset[ruleset_name]['rules'][rule_name] = {
+        defined_sniffs[rule_name] = {
             'name': rule_data[rule_name]['name'],
             'enabled': True,
             'description': rule_data[rule_name]['description'],
             'properties': {}
         }
 
-    #Get imported rules
-    for root, _, filenames in os.walk(sys.argv[1]):
-        for ruleset_file in fnmatch.filter(filenames, 'ruleset.xml'):
+    return defined_sniffs
 
-            tree = ElementTree.parse(os.path.join(root, ruleset_file))
-            root = tree.getroot()
 
-            ruleset_file_name = os.path.basename(ruleset_file)
-            ruleset_name = root.get('name')
+def get_imported_rules(standard_dir, rule_data):
+    imported_rules = {}
 
-            ruleset[ruleset_name]['name'] = ruleset_name
-            for rule_node in root.findall('rule'):
-                rule_name = rule_node.get('ref')
+    ruleset_file = os.path.join(standard_dir, 'ruleset.xml')
+    tree = ElementTree.parse(ruleset_file)
+    root = tree.getroot()
 
-                #Only pick "top-level" rules
-                if rule_name.count('.') == 2:
-                    ruleset[ruleset_name]['rules'][rule_name] = {
-                        'name': rule_data[rule_name]['name'],
-                        'enabled': True,
-                        'description': rule_data[rule_name]['description'],
-                        'properties': {}
-                    }
+    for rule_node in root.findall('rule'):
+        rule_name = rule_node.get('ref')
+
+        #Only pick "top-level" rules
+        if rule_name.count('.') == 2:
+            imported_rules[rule_name] = {
+                'name': rule_data[rule_name]['name'],
+                'enabled': True,
+                'description': rule_data[rule_name]['description'],
+                'properties': {}
+            }
+
+    return imported_rules
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Please call the script with the directory containing the PHPCS standards')
+        exit(0)
+
+    standards_path = sys.argv[1]
+    ruleset = {}
+
+    rule_data = get_rule_data()
+
+    EXCLUDED_STANDARDS = ['Generic', 'MySource', 'PHPCS']
+    standard_list = os.walk(standards_path).next()[1]
+
+    for standard in standard_list:
+        if standard not in EXCLUDED_STANDARDS:
+            standard_dir = os.path.join(standards_path, standard)
+
+            defined_sniffs = get_defined_sniffs(standard_dir, rule_data)
+            imported_rules = get_imported_rules(standard_dir, rule_data)
+
+            rules = {}
+            rules.update(defined_sniffs)
+            rules.update(imported_rules)
+
+            ruleset[standard] = {
+                'name': standard,
+                'rules': rules
+            }
 
     print(json.dumps(ruleset))
